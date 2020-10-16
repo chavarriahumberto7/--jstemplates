9043ff37ab437828664066432c08ae3b7249e401
Humberto Chavarria <estudios.humberto@gmail.com>
2020-10-16 12:08:07 -0600

    facebook update share

--- script.js
+++ script.js
@@ -54,19 +54,54 @@ async function getQuote() {
     }
 
 }
-
-function postQuote(){
+//this its to tweet the quote
+/*function postQuote(){
     const quote=quoteText.innerText;
     const author=authorText.innerText;
     const facebookUrl= `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
     window.open(facebookUrl,'_blank');
-}
+}*/
 
 //event Listeners
 newQuoteBtn.addEventListener('click',getQuote);
-facebootBtn.addEventListener('click',postQuote);
+//facebootBtn.addEventListener('click',postQuote);
 
 
 //On Load
 getQuote();
+$(function() {
+    /*Define some constants */
+    
+	const ARTICLE_TITLE =  document.title;
+	const ARTICLE_URL = encodeURIComponent(window.location.href);
+	const MAIN_IMAGE_URL = encodeURIComponent($('meta[property="og:image"]').attr('content'));
 
+	$(facebootBtn).click(function(){
+       
+		open_window('http://www.facebook.com/sharer/sharer.php?u='+ARTICLE_URL, authorText);
+	});
+
+	/* $('.share-twitter').click(function(){
+		open_window('http://twitter.com/share?url='+ARTICLE_URL, 'twitter_share');
+	});
+
+	$('.share-google-plus').click(function(){
+		open_window('https://plus.google.com/share?url='+ARTICLE_URL, 'google_share');
+	});
+
+	$('.share-linkedin').click(function(){
+		open_window('https://www.linkedin.com/shareArticle?mini=true&url='+ARTICLE_URL+'&title='+ARTICLE_TITLE+'&summary=&source=', 'linkedin_share');
+	});
+
+	$('.share-pinterest').click(function(){
+		open_window('https://pinterest.com/pin/create/button/?url='+ARTICLE_URL+'&media='+MAIN_IMAGE_URL+'&description='+ARTICLE_TITLE, 'pinterest_share');
+	});
+	
+	$('.share-tumblr').click(function(){
+		open_window('http://www.tumblr.com/share/link?url='+ARTICLE_URL+'&name='+ARTICLE_TITLE+'&description='+ARTICLE_TITLE, 'tumblr_share');
+	}); */
+
+	function open_window(url, name){
+		window.open(url, name, 'height=320, width=640, toolbar=no, menubar=no, scrollbars=yes, resizable=yes, location=no, directories=no, status=no');
+	}
+});
